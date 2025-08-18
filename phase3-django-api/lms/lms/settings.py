aaf26e2caacabf3e2a29b9ac9e10acd884e027a5
Sheikhsy <imreja9@gmail.com>
2025-08-18 10:12:50 +0530

    Updated

--- phase3-django-api/lms/lms/settings.py
+++ phase3-django-api/lms/lms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', #DRF
     'library',#app
+    'drf_yasg',
 
 ]
 
--- phase3-django-api/lms/lms/urls.py
+++ phase3-django-api/lms/lms/urls.py
@@ -17,9 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from library import urls as library_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Library Management System',
+        default_version='1.0.0',
+        description="API documentation for Library Management System"
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(library_urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
